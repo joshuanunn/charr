ad1809f23843fbd83f6ee95ec4639b3ffc1a2b24
Joshua Nunn <gh@joshua.nu>
2026-09-03 00:25:38 +0100

    Sign-off tests for chapter 4

--- test/oracles/chapter_04/valid/bitwise_or_precedence.s
+++ test/oracles/chapter_04/valid/bitwise_or_precedence.s
@@ -3,7 +3,6 @@
 main:
     pushq       %rbp
     movq        %rsp, %rbp
-    subq        $16, %rsp
     movl        $5, %eax
     movq        %rbp, %rsp
     popq        %rbp
--- test/oracles/chapter_04/valid/or_true.s
+++ test/oracles/chapter_04/valid/or_true.s
@@ -3,7 +3,6 @@
 main:
     pushq       %rbp
     movq        %rsp, %rbp
-    subq        $32, %rsp
     movl        $3, %eax
     movq        %rbp, %rsp
     popq        %rbp
