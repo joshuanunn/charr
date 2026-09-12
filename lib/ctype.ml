06dfabe00de77c6503da96c8bad135300b1fa208
Joshua Nunn <gh@joshua.nu>
2026-09-12 11:08:44 +0100

    Add support for unsigned int and unsigned long to parser

--- lib/ctype.ml
+++ lib/ctype.ml
@@ -1,7 +1,14 @@
-type t = Int | Long | FunType of { params : t list; ret : t }
+let uint_max = 0xFFFF_FFFFL
+
+type t = Int | Long | UInt | ULong | FunType of { params : t list; ret : t }
 [@@deriving show, ord, eq]
 
-type const = ConstInt of int32 | ConstLong of int64 [@@deriving show]
+type const =
+  | ConstInt of int32
+  | ConstLong of int64
+  | ConstUInt of int32
+  | ConstULong of int64
+[@@deriving show]
 
 let const_convert (target : t) (c : const) : const =
   match (target, c) with
@@ -13,21 +20,35 @@ let const_convert (target : t) (c : const) : const =
   | Long, ConstLong l -> ConstLong l
   | FunType _, _ ->
       failwith "internal error: cannot convert constant to function type"
+  | _ -> failwith "TODO"
 
 let const_one = function
   | Int -> ConstInt 1l
   | Long -> ConstLong 1L
+  | UInt -> ConstUInt 1l
+  | ULong -> ConstULong 1L
   | FunType _ -> failwith "internal error: no unit constant for function type"
 
 type static_init = IntInit of int32 | LongInit of int64 [@@deriving show]
 
-let to_int64 = function ConstInt i -> Int64.of_int32 i | ConstLong l -> l
+let to_int64 = function
+  | ConstInt i -> Int64.of_int32 i
+  | ConstLong l -> l
+  | ConstUInt i -> Int64.logand (Int64.of_int32 i) uint_max
+  | ConstULong l -> l
 
 let of_int64 t v =
   match t with
   | Int -> ConstInt (Int64.to_int32 v)
   | Long -> ConstLong v
+  | UInt -> ConstUInt (Int64.to_int32 v)
+  | ULong -> ConstULong v
   | FunType _ -> failwith "internal error: constant with function type"
 
-let const_type = function ConstInt _ -> Int | ConstLong _ -> Long
+let const_type = function
+  | ConstInt _ -> Int
+  | ConstLong _ -> Long
+  | ConstUInt _ -> UInt
+  | ConstULong _ -> ULong
+
 let is_zero c = to_int64 c = 0L
--- lib/parser.mly
+++ lib/parser.mly
@@ -75,6 +75,10 @@
 
 %start <Ast.prog> prog
 
+(* Type inference is fragile for optional lists, so declare upfront *)
+%type <(Ctype.t * Ast.ident) list> param_list_opt
+%type <Ast.expr list> arg_expr_list_opt
+
 %%
 
 prog:
@@ -97,8 +101,10 @@ specifier_list:
   ;
 
 type_specifier:
-  | KW_INT { Ctype.Int }
-  | KW_LONG { Ctype.Long }
+  | KW_INT { Ast.TSInt }
+  | KW_LONG { Ast.TSLong }
+  | KW_SIGNED { Ast.TSSigned }
+  | KW_UNSIGNED { Ast.TSUnsigned }
   ;
 
 specifier:
@@ -323,4 +329,6 @@ identifier:
 constant:
   | LITERAL_INT { Ast.mk_int_const $1 }
   | LITERAL_LONG { Ast.mk_long_const $1 }
+  | LITERAL_UINT { Ast.mk_uint_const $1 }
+  | LITERAL_ULONG { Ast.mk_ulong_const $1 }
   ;
